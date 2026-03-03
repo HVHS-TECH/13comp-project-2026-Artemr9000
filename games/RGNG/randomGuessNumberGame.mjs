d12b3815561aef488ce05a59144a1712fc044dfc
artemr9000 <22155ar@hvhs.school.nz>
2026-03-04 12:30:33 +1300

    started random guess js

--- games/RGNG/randomGuessNumberGame.mjs
+++ games/RGNG/randomGuessNumberGame.mjs
@@ -3,3 +3,11 @@
 // Main game logic for Bacteria Game
 // Written by Artem Rakhimov, Term 1 2026
 /**************************************************************/
+var winningNumber = Math.floor(Math.random() * 10) + 1;
+
+
+function randomNumberGame(){
+
+
+
+};
